14bca9b1fefc10b7d58aeb723da5df66d566377e
irem ates <irematees16@gmail.com>
2024-04-13 23:25:28 +0300

    Add category name

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -4,7 +4,6 @@ import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import Product from '@/components/Product.vue'
 
-// const page = ref()
 const category = ref()
 const route = useRoute()
 
@@ -17,7 +16,12 @@ onMounted(() => {
 watch(() => route.params.categoryId, fetchPage)
 </script>
 <template>
-  <div class="container mx-auto grid grid-cols-5 w-3/4 gap-10 py-8" v-if="category">
-    <Product v-for="(product, index) in category" :key="index" :product="product" />
+  <div class="container mx-auto w-3/4 pt-6">
+    <h1>
+      "{{ category[1].category.title }}" kategorisi için {{ category.length }} sonuç listeleniyor
+    </h1>
+    <div class="container mx-auto grid grid-cols-5 w-full gap-10 py-8" v-if="category">
+      <Product v-for="(product, index) in category" :key="index" :product="product" />
+    </div>
   </div>
 </template>
